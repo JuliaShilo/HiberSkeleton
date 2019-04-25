e1f79dc99edda414d05f97fff046f3795cd8f56d
Alex <ignatenko2207@gmail.com>
2019-04-25 20:33:45 +0300

    Remove autogeneration of id

--- src/main/java/org/itstep/entity/Cart.java
+++ src/main/java/org/itstep/entity/Cart.java
@@ -20,7 +20,7 @@ import lombok.Setter;
 public class Cart {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Integer id;
 	
--- src/main/java/org/itstep/entity/Order.java
+++ src/main/java/org/itstep/entity/Order.java
@@ -20,7 +20,7 @@ import lombok.Setter;
 public class Order {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private Integer id;
 	
