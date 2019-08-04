f00f484fbe857e801217e03e8e069bb54a7ed6c4
JuliaShilo <akira766@mail.ru>
2019-08-04 23:18:53 +0300

    add pg

--- src/main/java/org/itstep/dao/HibernateFactory.java
+++ src/main/java/org/itstep/dao/HibernateFactory.java
@@ -24,16 +24,27 @@ public class HibernateFactory {
 
         		Properties properties = new Properties();
 
-        		properties.setProperty("hibernate.connection.driverClassName", "org.h2.Driver");
-        		properties.setProperty("hibernate.connection.url", "jdbc:h2:~/step_qa_2");
-        		properties.setProperty("hibernate.connection.username", "julia");
-        		properties.setProperty("hibernate.connection.password", "123456");
-        		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-        		properties.setProperty("hibernate.hbm2ddl.auto", "update");
-        		properties.setProperty("hibernate.show_sql", "true");
-//        		properties.setProperty("hibernate.format_sql", "true");
+//        		properties.setProperty("hibernate.connection.driverClassName", "org.h2.Driver");
+//        		properties.setProperty("hibernate.connection.url", "jdbc:h2:~/step_qa_2");
+//        		properties.setProperty("hibernate.connection.username", "julia");
+//        		properties.setProperty("hibernate.connection.password", "123456");
+//        		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+//        		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+//        		properties.setProperty("hibernate.show_sql", "true");
+////        		properties.setProperty("hibernate.format_sql", "true");
 
-        		configuration.addProperties(properties);
+
+				properties.setProperty("hibernate.connection.driverClassName", "org.postgresql.Driver");
+				properties.setProperty("hibernate.connection.url", "jdbc:postgresql://localhost:5432/step-qa-2");
+				properties.setProperty("hibernate.connection.username", "postgres");
+				properties.setProperty("hibernate.connection.password", "feeder456");
+				properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+				properties.setProperty("hibernate.hbm2ddl.auto", "update");
+				properties.setProperty("hibernate.show_sql", "true");
+				properties.setProperty("hibernate.format_sql", "true");
+
+
+				configuration.addProperties(properties);
 
         		configuration.addAnnotatedClass(Item.class);
         		configuration.addAnnotatedClass(User.class);
